10ffe99214bd77d9cb6d9972e2773be051230804
Nurislam <nurislam_96@mail.ru>
2022-05-24 23:16:50 +0600

    initial commit

---
+++ apps/product/admin.py
@@ -0,0 +1,44 @@
+from csv import list_dialects
+from re import search
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','id']
+    prepopulated_fields = {"slug":('name',)}
+
+
+@admin.register(SubCategory)
+class SubCategory(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'slug',
+        'id'
+        )
+    prepopulated_fields = {"slug":('name',)}
+    list_filter = (
+        "category",
+    )
+    search_fields = (
+        "id",
+        "name",
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'image',
+        'category',
+        'subcategory',
+        'created',
+        'updated',
+        'is_active'
+    ]
---
+++ apps/product/views.py
@@ -0,0 +1,59 @@
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView
+)
+
+import json
+# Create your views here.
+from .models import SubCategory, Category , Product
+
+
+def get_subcategory(request):
+    id = request.GET.get('id', '')
+    result = list(SubCategory.objects.filter(
+    category_id=int(id)).values('id', 'name'))
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+class MainPage(TemplateView):
+    template_name = "base.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     baners = BanerImage.objects.all()
+    #     if len(baners) > 6:
+    #         baners = baners[:6]
+    #     context['baners'] = baners
+    #     return context
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    paginate_by = 1
+    #стандартное имя списка продуктов в шаблоне
+    # для ListView - object_list
+
+    def get_queryset(self):
+        print(self.kwargs)
+        category_slug = self.kwargs.get('slug')
+        subcategory_slug = self.kwargs.get('subcategory_slug')
+        print(category_slug)
+        print(subcategory_slug)
+        if subcategory_slug:
+            products = Product.objects.filter(is_active=True, subcategory__slug=subcategory_slug)
+        elif category_slug:
+            products = Product.objects.filter(is_active=True, category__slug=category_slug)
+        else:
+            products = Product.objects.filter(is_active=True)
+        return products
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product" # стандартный object
